da7cbb88e62f7905aa14e4df3fb0738a276c48fe
나승일 <nsi13748@naver.com>
2023-07-19 14:07:16 +0900

    Post 목록 샘플 데이터 준비 -> Model (DTO) 클래스 생성

---
+++ lib/api/api_info.dart
@@ -0,0 +1,5 @@
+class ApiInfo {
+
+  static const String basePostUrl = 'https://jsonplaceholder.typicode.com';
+
+}
---
+++ lib/dto/post_model.dart
@@ -0,0 +1,26 @@
+class PostModel {
+
+  int userId ;
+  int id ;
+  String title;
+  String body ;
+
+  // 가능한 기본값을 줘서 예상치 못한 상황을 대비해야 한다.
+  PostModel({
+    this.userId = -1,
+    this.id = -1,
+    this.title = '',
+    this.body = '',
+})
+
+  factory PostModel.fromJson(Map<String, dynamic> json) {
+    return PostModel(
+      userId: json['userId'] ?? -1,
+      id: json['id'] ?? -1 ,
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+
+
+}
